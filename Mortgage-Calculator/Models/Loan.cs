a8588d7422919eda7f0e2f4f40d180f1f01b6073
meganlynn21 <meganlkeyes0203@gmail.com>
2022-04-02 21:08:50 -0400

    Created Models and new classes

---
+++ Mortgage-Calculator/Models/Loan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mortgage_Calculator.Models
+{
+    public class Loan
+    {
+        public decimal Amount { get; set; }
+
+        public decimal Rate { get; set; }
+
+        public int Term { get; set; }
+
+        public decimal Payment { get; set; }
+
+        public decimal TotalInterest { get; set; }
+
+        public decimal TotalCost { get; set; }
+
+        public List<LoanPayment> Payments { get; set; } = new List<LoanPayment>();
+    }
+}
---
+++ Mortgage-Calculator/Models/LoanPayment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mortgage_Calculator.Models
+{
+    public class LoanPayment
+    {
+        public int Month { get; set; }
+
+        public decimal Payment { get; set; }
+
+        public decimal MonthlyPrincipal { get; set; }
+
+        public decimal MonthlyInterest { get; set; }
+
+        public decimal TotalInterest { get; set; }
+
+        public decimal Balance { get; set; }
+    }
+}
